d46f5dd4cd2b56214006b04d5e56ae2ec64cdae1
Ali Nowrouzi <alinowrouzii@gmail.com>
2022-08-27 23:04:03 +0430

    Add yml parser and much more logic

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v3 v3.0.1
 )
--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"sync"
 
 	tinymux "github.com/alinowrouzii/tiny-mux"
 	"github.com/hpcloud/tail"
+	"gopkg.in/yaml.v3"
 )
 
 type MetricHandler struct {
@@ -19,6 +21,21 @@ type MetricHandler struct {
 	l sync.Mutex
 	// for instance: methods['GET'][400] gives the number of requests with method=GET and status code 400
 	methods *map[string]map[string]int64
+}
+
+type YamlConfig struct {
+	Main struct {
+		Listen int    `yaml:"listen"`
+		Route  string `yaml:"route"`
+	} `yaml:"main"`
+	Apps []struct {
+		AppName interface{} `yaml:"app-name"`
+		Logs    []string    `yaml:"logs"`
+		Methods struct {
+			GET  []int `yaml:"GET"`
+			POST []int `yaml:"POST"`
+		} `yaml:"methods"`
+	} `yaml:"apps"`
 }
 
 func proccessData(line string) (string, string, string) {
@@ -39,22 +56,29 @@ func (h *MetricHandler) metricsHandler(w http.ResponseWriter, r *http.Request) {
 
 	// here we inform our goroutine calculator to does not enter to our critical section if
 	// handler wants to show the result to the client
-	h.l.Lock()
 	h.n.Lock()
 	h.m.Lock()
 	h.n.Unlock()
-	// fmt.Println("Locking process for 10 seconds")
-	// time.Sleep(time.Second * 10)
+	lines := ""
+	lines += fmt.Sprintf("# HELP my_nginx_log_exporter_requests_total Number of request with specified status and method.\n")
+	lines += fmt.Sprintf("# TYPE my_nginx_log_exporter_requests_total counter\n")
+	for method := range *h.methods {
+		for status := range (*h.methods)[method] {
+			// fmt.Printf("method=%s   status=%s    value=%d\n\n", method, status, (*h.methods)[method][status])
+			lines += fmt.Sprintf("my_nginx_log_exporter_requests_total{method=\"%s\", status=\"%s\"} %d\n", method, status, (*h.methods)[method][status])
+		}
+	}
+	w.Write([]byte(lines))
 	h.m.Unlock()
-	h.l.Unlock()
-	w.Write([]byte("helllllo ali"))
 }
 
 func main() {
+
 	methods := map[string]map[string]int64{
 		"GET": {
 			"400": 0,
 			"500": 0,
+			"200": 0,
 		},
 		"POST": {
 			"200": 0,
@@ -76,7 +100,6 @@ func main() {
 	go func() {
 		t, _ := tail.TailFile("/var/log/nginx/access.log", tail.Config{Follow: true})
 		for line := range t.Lines {
-			fmt.Println("Im stock here")
 			status, method, _ := proccessData(line.Text)
 			if _, ok := (*metric.methods)[method]; ok {
 				if _, ok := (*metric.methods)[method][status]; ok {
@@ -85,24 +108,21 @@ func main() {
 					metric.n.Lock()
 					metric.m.Lock()
 					metric.n.Unlock()
-					// fmt.Println("before log")
-					// // time.Sleep(time.Second * 10)
-					// fmt.Println("after log")
-					// fmt.Println("Sleeping for ten seconds...")
+
 					(*metric.methods)[method][status]++
 
 					metric.m.Unlock()
 					metric.l.Unlock()
 				}
 			}
-
-			fmt.Println((*metric.methods)["GET"]["500"])
 		}
 	}()
 
 	mux.GET("/metrics", http.HandlerFunc(metric.metricsHandler))
 
 	http.ListenAndServe(":8888", mux)
+
+	// parseYml()
 }
 
 func splitQoutes(s string) []string {
@@ -114,4 +134,123 @@ func splitQoutes(s string) []string {
 		return r == '"' || (!insideQoute && r == ' ')
 	})
 	return out
+}
+
+var data = `
+a: Easy!
+b:
+  c: 2
+  d: [3, 4]
+`
+
+type App struct {
+	Logs    []string `yaml:"logs"`
+	Methods struct {
+		GET  []int `yaml:"GET"`
+		POST []int `yaml:"POST"`
+	} `yaml:"methods"`
+}
+
+func parseYml() {
+	parsedYml := make(map[interface{}]interface{})
+	data, err := ioutil.ReadFile("./config.yml")
+
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(data, &parsedYml)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(parsedYml["apps"])
+
+	apps, ok := parsedYml["apps"]
+
+	if !ok {
+		panic("invalid yml format!")
+	}
+
+	main, ok := parsedYml["main"]
+	parsedListen := "8888"
+	parsedRoute := "metrics"
+	if ok {
+		parsedMain, ok := main.(map[string]interface{})
+		if !ok {
+			panic("invalid yaml format")
+		}
+
+		listen, ok := parsedMain["listen"]
+		if ok {
+			parsedListen, ok = listen.(string)
+			if !ok {
+				panic("invalid yaml format")
+			}
+		}
+
+		route, ok := parsedMain["route"]
+		if ok {
+			parsedRoute, ok = route.(string)
+			if !ok {
+				panic("invalid yaml format")
+			}
+		}
+	}
+
+	for appName := range apps.(map[string]interface{}) {
+
+		app := apps.(map[string]interface{})[appName].(map[string]interface{})
+		logs, ok := app["logs"]
+		if !ok {
+			panic("invalid yaml format")
+		}
+
+		parsedLogs := make([]string, 0)
+		switch t := logs.(type) {
+		case []interface{}:
+			for _, value := range t {
+				castedValue, ok := value.(string)
+				if !ok {
+					panic("invalid yaml format")
+				}
+				parsedLogs = append(parsedLogs, castedValue)
+			}
+		default:
+			panic("invalid yaml format")
+		}
+
+		fmt.Println("parsedLogs: ", parsedLogs)
+		methods, ok := app["methods"]
+		if !ok {
+			panic("invalid yaml format")
+		}
+
+		parsedMethods, ok := methods.(map[string]interface{})
+		if !ok {
+			panic("invalid yaml format")
+		}
+
+		allMethdos := make(map[string][]string)
+		for methodName := range parsedMethods {
+			switch t := parsedMethods[methodName].(type) {
+			case []interface{}:
+				for _, value := range t {
+					castedValue, ok := value.(string)
+					if !ok {
+						panic("invalid yaml format")
+					}
+					allMethdos[methodName] = append(allMethdos[methodName], castedValue)
+				}
+			default:
+				panic("invalid yaml format")
+			}
+		}
+
+		fmt.Println(allMethdos)
+		fmt.Println(parsedLogs)
+	}
+
+	fmt.Println(parsedListen)
+	fmt.Println(parsedRoute)
+
 }
